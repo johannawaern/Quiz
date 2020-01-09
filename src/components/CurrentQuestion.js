bce02b0e53fe6378be1704302da520fa20384f79
ucha3i <jchabik@gmail.com>
2020-01-09 14:30:05 +0100

    validation on first question

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -5,7 +5,15 @@ import { quiz } from 'reducers/quiz'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const answer = useSelector((state) => state.quiz.answers.find((a) => a.questionId === question.id))
+  console.log(answer)
   const dispatch = useDispatch()
+
+  /* const handleOnClick = () => {
+    if (quiz.answers.question.isCorrect === true) {
+      return console.log("hey")
+    }
+  } */
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
@@ -13,11 +21,14 @@ export const CurrentQuestion = () => {
 
   return (
     <>
+      {answer && <h1>Hey {answer.isCorrect ? 'Correct' : 'Wrong'}</h1>}
       <h1>Question: {question.questionText}</h1>
       {question.options.map((option, index) => (
         <button
           onClick={() => {
-            dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
+            if (!answer) {
+              dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
+            }
           }}
           key={option}>
           {option}
