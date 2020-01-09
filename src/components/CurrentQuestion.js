0b90ab8581b94299c13ae430606af80c83d459c4
ucha3i <jchabik@gmail.com>
2020-01-09 13:37:39 +0100

    second commit - still first  question

--- src/components/CurrentQuestion.js
+++ src/components/CurrentQuestion.js
@@ -1,9 +1,11 @@
 /* eslint-disable react/button-has-type */
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from 'reducers/quiz'
 
 export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
+  const dispatch = useDispatch()
 
   if (!question) {
     return <h1>Oh no! I could not find the current question!</h1>
@@ -12,7 +14,15 @@ export const CurrentQuestion = () => {
   return (
     <>
       <h1>Question: {question.questionText}</h1>
-      {question.options.map((option) => <button key={option}>{option}</button>)}
+      {question.options.map((option, index) => (
+        <button
+          onClick={() => {
+            dispatch(quiz.actions.submitAnswer({ questionId: question.id, answerIndex: index }))
+          }}
+          key={option}>
+          {option}
+        </button>
+      ))}
     </>
   )
 }
