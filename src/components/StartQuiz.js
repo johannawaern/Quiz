b1ad9c1f07e51a3b13f791f18f52e00997a3b5d5
Johanna Luks Wærn <johanna@misswaern.se>
2020-01-10 09:51:39 +0100

    test

--- src/components/StartQuiz.js
+++ src/components/StartQuiz.js
@@ -6,4 +6,15 @@ export const CurrentQuestion = () => {
   const question = useSelector((state) => state.quiz.questions[state.quiz.currentQuestionIndex])
   const answer = useSelector((state) => state.quiz.answers.find((a) => a.questionId === question.id))
   const dispatch = useDispatch()
-  const isLastQuestion = useSelector((state) => state.quiz.questions.length - state.quiz.answers.length === 0)
+ 
+
+
+return (
+  <button
+  onClick={() => {
+    dispatch(quiz.actions.goToNextQuestion())
+  }}>
+  Start the quiz!
+</button>}
+)
+}
