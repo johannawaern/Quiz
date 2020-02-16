47ff6643908b6c93ff678059c308e908e4aeb709
Johanna Luks Wærn <johanna@misswaern.se>
2020-02-16 16:56:18 +0100

    updated "" to ''

--- src/components/NextOrSubmit.js
+++ src/components/NextOrSubmit.js
@@ -1,10 +1,10 @@
-import React from "react"
-import { useSelector, useDispatch } from "react-redux"
-import { quiz } from "reducers/quiz"
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { quiz } from 'reducers/quiz'
 
 export const NextOrSubmit = () => {
   const isLastQuestion = useSelector(
-    state => state.quiz.questions.length - state.quiz.answers.length === 0
+    (state) => state.quiz.questions.length - state.quiz.answers.length === 0
   )
   const dispatch = useDispatch()
 
@@ -14,9 +14,8 @@ export const NextOrSubmit = () => {
       className='nextSubmitButton'
       onClick={() => {
         dispatch(quiz.actions.goToNextQuestion())
-      }}
-    >
-      {isLastQuestion ? "Submit" : "Next question"}
+      }}>
+      {isLastQuestion ? 'Submit' : 'Next question'}
     </button>
   )
 }
